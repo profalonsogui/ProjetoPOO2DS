a2192d625abfd9d67d05349b169c7386f02ced91
Professor Guilherme <guilhermealonsoribeiro@gmail.com>
2026-03-19 12:19:12 -0300

    teste iniciais

--- Program.cs
+++ Program.cs
@@ -22,7 +22,8 @@ class Program
         p1.Idade = int.Parse(Console.ReadLine()!);
 
         p1.Apresentar();
-        prod1.mostrarProduto();
+        Console.WriteLine($"Nome produto: {prod1.Nome}");
+        Console.WriteLine($"Preço produto: {prod1.Preco}");
 
         Console.WriteLine($"O cliente {p1.Nome} comprou o produto: {prod1.Nome} e pagou R${prod1.Preco}.");
     }
