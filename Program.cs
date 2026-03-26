2ef594b2f006f505904b954577a636e989ad91ae
Professor Guilherme <guilhermealonsoribeiro@gmail.com>
2026-03-26 13:35:36 -0300

    atualização do Program.cs

--- Program.cs
+++ Program.cs
@@ -2,18 +2,18 @@
 
 Console.WriteLine("--- Cadastro Escolar ---");
 Console.Write("Digite o Nome: ");
-string nomeInput = Console.ReadLine();
+string nomeInput = Console.ReadLine()!;
 
 Console.Write("Digite a Idade: ");
-int idadeInput = int.Parse(Console.ReadLine());
+int idadeInput = int.Parse(Console.ReadLine()!);
 
 Console.WriteLine("O que deseja cadastrar? [1] Aluno | [2] Professor");
-string escolha = Console.ReadLine();
+string escolha = Console.ReadLine()!;
 
 if (escolha == "1")
 {
     Console.Write("Digite a Matrícula: ");
-    string mat = Console.ReadLine();
+    string mat = Console.ReadLine()!;
 
     Aluno a = new Aluno(nomeInput, idadeInput, mat);
     a.ExibirAluno();
@@ -21,7 +21,7 @@ if (escolha == "1")
 else if (escolha == "2")
 {
     Console.Write("Digite a Disciplina: ");
-    string disc = Console.ReadLine();
+    string disc = Console.ReadLine()!;
 
     Professor p = new Professor(nomeInput, idadeInput, disc);
     p.ExibirProfessor();
